3e3119623d39a5454bb5f63896e9c4937b08ee5a
Yevgeniy <evgeniyv6@gmail.com>
2021-06-11 17:16:28 +0300

    add rationale for choosing a router

--- app/main.go
+++ app/main.go
@@ -5,13 +5,8 @@ import (
 	"log"
 	"math"
 
-	"github.com/evgeniyv6/go_backend_shorturl/app/internal/configuration"
 	"github.com/evgeniyv6/go_backend_shorturl/app/internal/hasher"
-
-	"github.com/gomodule/redigo/redis"
 )
-
-var FSO configuration.OsFileSystem
 
 func main() {
 	fmt.Println("hello from custom bitly. Clear this file when complete.")
@@ -19,27 +14,5 @@ func main() {
 
 	log.Println(string(hasher.GenHash(uint64(math.MaxUint64))))
 	log.Println(hasher.GenClear("pIrkgbKrQ8v"))
-
-	// try redis
-	conn, err := redis.Dial("tcp", "127.0.0.1:6379")
-	if err != nil {
-		log.Printf("Connect redis err: %s", err)
-	}
-	log.Println("Success redis connect!")
-	defer func() {
-		err := conn.Close()
-		if err != nil {
-			log.Printf("Close redis err: %s\n", err)
-		}
-	}()
-
-	rep, err := conn.Do("ping")
-	if err != nil {
-		log.Printf("Cmd DO err: %s", err)
-	}
-	log.Println(rep)
-
-	c, err := configuration.ReadConfig("config.json", FSO)
-	log.Println(c)
 
 }
